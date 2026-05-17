164922f244709a22a6eedfa15a35fad7dcfb4ce9
Melika Shooryabi <138528779+Melikash98@users.noreply.github.com>
2026-05-17 16:14:39 +0330

    Update

---
+++ src/main/java/com/melikash98/AdminSpring/AuthController.java
@@ -0,0 +1,30 @@
+package com.melikash98.AdminSpring;
+
+import com.melikash98.AdminSpring.Model.LoginRequest;
+import com.melikash98.AdminSpring.Model.RegisterRequest;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/auth")
+public class AuthController {
+
+    private final AuthService authService;
+
+    public AuthController(AuthService authService) {
+        this.authService = authService;
+    }
+
+    @PostMapping("/register")
+    public ResponseEntity<String> register(@RequestBody RegisterRequest request) {
+        return ResponseEntity.ok(authService.register(request));
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<String> login(@RequestBody LoginRequest request) {
+        return ResponseEntity.ok(authService.login(request));
+    }
+}
