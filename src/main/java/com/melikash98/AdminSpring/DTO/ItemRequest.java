d9b13ef2d2b7a282977ac1d998176abc7a885ad8
Melika Shooryabi <138528779+Melikash98@users.noreply.github.com>
2026-05-21 14:49:26 +0330

    Update

--- src/main/java/com/melikash98/AdminSpring/DTO/ItemRequest.java
+++ src/main/java/com/melikash98/AdminSpring/DTO/ItemRequest.java
@@ -1,5 +1,6 @@
 package com.melikash98.AdminSpring.DTO;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
 
 import java.util.List;
@@ -7,16 +8,25 @@ import java.util.List;
 @Data
 public class ItemRequest {
     private String name;
+
+    @JsonProperty("overView")
     private String overview;
-    private String price;
+
+    @JsonProperty("types")
     private String typeItems;
+
+    private String price;
     private String discount;
     private String deliveryPrice;
     private boolean freeDelivery;
     private String categoryId;
     private String adminId;
+    private String location;
+
+    @JsonProperty("photos")
     private List<String> photos;
-    private boolean isLiked;
+
     private String city;
     private String country;
+    private boolean isLiked;
 }
